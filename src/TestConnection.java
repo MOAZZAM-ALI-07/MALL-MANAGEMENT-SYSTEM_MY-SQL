ac6a67150856e162978ab469c977bae86a90951d
MOAZZAM-ALI-07 <moazzamali2254@gmail.com>
2026-01-26 18:39:04 +0500

    Final Project Upload: Mall Management System with Database and Images.

---
+++ src/TestConnection.java
@@ -0,0 +1,20 @@
+import util.DBConnection;
+import java.sql.Connection;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+            Connection con = DBConnection.getConnection();
+            System.out.println("Database Connected!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
